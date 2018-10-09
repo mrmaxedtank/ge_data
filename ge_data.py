8f6f6d92378027be7085dade40e0db609b85382d
mrmaxedtank <43934408+mrmaxedtank@users.noreply.github.com>
2018-10-09 09:30:34 +0200

    Added source to osb data

--- ge_data.py
+++ ge_data.py
@@ -2,15 +2,21 @@ import requests
 import string
 import json
 import pg
+import urllib
 
-url1 = 'http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item='
-url2 = ''
+#Fetch osbuddy summary
+json_url = 'https://rsbuddy.com/exchange/summary.json'
+response = urllib.urlopen(json_url)
+osb_data = json.load(response.read())
+
+url = 'http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item='
 
 connection = pg.DB(host=<hostname>, user=<username>, passwd=<password>, dbname=<dbname>)
 items = connection.query("select item_id from items")
 connection.close()
 
 for item_id in items:
-    official = url1 + item_id
-    osbuddy = url2 + item_id
+    official = url + item_id
+    #Below, take right element from "osb_data". item_id can be used to identify the right element.
+    osbuddy = 
     
