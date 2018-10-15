daaafbb92c842d24a1bc058ad57314f7d22d806d
mrmaxedtank <43934408+mrmaxedtank@users.noreply.github.com>
2018-10-15 14:36:13 +0200

    Update ge_data.py

--- ge_data.py
+++ ge_data.py
@@ -31,20 +31,27 @@ for ItemId in Items:
     #HERE: parse official_data, compare price with last result for specific item.
     #If different; register new price in new record and set GeUpdated to True
     #If equal; next item    
-    OfficialLastPrice = connection.execute('select limit 1 price from records where source = ? and item_id = ? order by record_datetime desc', ('osbuddy', ItemId))
-    OfficialNewPrice = print(OsbData[ItemId]['overall_average']
+    OfficialLastPrice = connection.execute('select limit 1 price from records where source = ? and item_id = ? order by record_datetime desc', ('osrs', ItemId))
+    OfficialNewPrice = 
     if OfficialLastPrice <> OfficialNewPrice
         #Insert record into database
-        
+        #Todo: Check if ID works like this, create variable for timestamp of "now". Create and test this before taking care of osbuddy segment.
+        connection.execute('INSERT INTO records (id, record_datetime, item_id, price, source' VALUES (?, ?, ?, ?, ?)', ('', '', ItemId, OfficialNewPrice, 'osrs'))
         GeUpdated = True
     else
+        #Add logging for else?
     break
     
     #Below, take right element from "osb_data". item_id can be used to identify the right element.
-    Osbuddy = 
+    OsbuddyLastPrice = connection.execute('select limit 1 price from records where source = ? and item_id = ? order by record_datetime desc', ('osbuddy', ItemId))
+    Osbuddy = print(OsbData[ItemId]['overall_average'])
     #Compare osbuddy price to last osbuddy price
+    if OsbuddyLastPrice <> Osbuddy
     #If different; register new price
     #If equal; next    
+    else
+        #Add logging for else?
+    break
 break
 
 if GeUpdated == True
